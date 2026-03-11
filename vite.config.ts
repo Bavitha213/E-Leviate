13e5496302424fb48956759a6a08d5cebd7d6ce0
Bavitha <136009415+Bavitha213@users.noreply.github.com>
2026-03-11 22:02:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/E-Leviate/',
+  base: './',   // important for Netlify
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
-});
+  build: {
+    outDir: 'dist'
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react']
+  }
+})
