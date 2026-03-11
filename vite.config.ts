85b20c4173c2cc8c2b6d789112949fe50d34bc4f
Bavitha <136009415+Bavitha213@users.noreply.github.com>
2026-03-11 21:32:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/E-Leviate/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
